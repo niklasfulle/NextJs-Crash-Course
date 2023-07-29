338184874e8f023f32f39d8e25498f716887c11e
niklasfulle <fulle.niklas@web.de>
2023-07-29 21:21:28 +0200

    Chapter 8: Build a REST API

---
+++ Chapter 8 Build a REST API/src/app/api/todos/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+
+const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
+
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
+export async function GET() {
+  const res = await fetch(DATA_SOURCE_URL);
+
+  const todos: Todo[] = await res.json();
+
+  return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+  const { title, userId }: Partial<Todo> = await request.json();
+
+  if (!userId || !title) return NextResponse.json({ "message": "Missing requierd data" })
+
+  const res = await fetch(`${DATA_SOURCE_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': API_KEY
+    },
+    body: JSON.stringify({ title, userId, completed: false }),
+  })
+
+  const newTodo: Todo = await res.json()
+
+  return NextResponse.json(newTodo, { status: 201 })
+}
+
+export async function PUT(request: Request) {
+  const { userId, id, title, completed }: Todo = await request.json();
+
+  if (!userId || !id || !title || typeof (completed) !== "boolean") return NextResponse.json({ "message": "Missing requierd data" })
+
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': API_KEY
+    },
+    body: JSON.stringify({ title, userId, completed }),
+  })
+
+  const updatedTodo: Todo = await res.json()
+
+  return NextResponse.json(updatedTodo, { status: 200 })
+}
+
+export async function DELETE(request: Request) {
+  const { id }: Partial<Todo> = await request.json()
+
+  if (!id) return NextResponse.json({ "message": "Todo id required" })
+
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': API_KEY
+    }
+  })
+
+  return NextResponse.json({ "message": `Todo ${id} deleted` })
+}
