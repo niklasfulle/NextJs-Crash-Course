62176187e3efb5ad92b49a3f955d358f4ee11c4c
niklasfulle <fulle.niklas@web.de>
2023-07-29 20:26:30 +0200

    Chapter 7: Route Handlers

---
+++ Chapter 7, 8, 9 & 11 /src/app/api/echo/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  //const name = searchParams.get('name')
+  //const instrument = searchParams.get('instrument')
+
+  const obj = Object.fromEntries(searchParams.entries());
+
+  return NextResponse.json({ obj })
+}
