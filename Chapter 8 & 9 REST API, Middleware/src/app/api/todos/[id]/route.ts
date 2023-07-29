461f4b772bf8fc33ffe2d329e15901ae024d80dd
niklasfulle <fulle.niklas@web.de>
2023-07-29 21:27:06 +0200

    changed folder name

--- Chapter 8 & 9 REST API, Middleware/src/app/api/todos/[id]/route.ts
+++ Chapter 8 & 9 REST API, Middleware/src/app/api/todos/[id]/route.ts
@@ -2,8 +2,15 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+type Props = {
+  params: {
+    id: string;
+  }
+}
+
+export async function GET(request: Request, { params: { id } }: Props) {
+  //const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
   const todo: Todo = await res.json();
